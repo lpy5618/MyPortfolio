55d229b620398d2486a0c4405caedd7e2545fbd0
PUYU LI (Jeff) <lpy547898@gmail.com>
2024-04-10 12:00:49 +1000

    banner finished

--- myportfolio/src/components/Banner.js
+++ myportfolio/src/components/Banner.js
@@ -8,17 +8,42 @@ export const Banner = () => {
     const [isDeleting, setIsDeleting] = useState(false);
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
-    const toRotate = ['AI Developer', 'Machine Learning Engineer', 'Data Scientist', 'Software Developer'];
+    const [index, setIndex] = useState(1);
+    const toRotate = ['AI/ML Developer', 'Web developer', 'Data Scientist', 'Software Developer'];
     const period = 2000;
 
     useEffect(() => {
         let ticker = setInterval(() => {
-            ticker();
+          tick();
         }, delta);
-        return () => {
-            clearInterval(ticker)
-        };
-    }, [text]);
+    
+        return () => { clearInterval(ticker) };
+      }, [text])
+    
+      const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    
+        setText(updatedText);
+    
+        if (isDeleting) {
+          setDelta(prevDelta => prevDelta / 2);
+        }
+    
+        if (!isDeleting && updatedText === fullText) {
+          setIsDeleting(true);
+          setIndex(prevIndex => prevIndex - 1);
+          setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+          setIsDeleting(false);
+          setLoopNum(loopNum + 1);
+          setIndex(1);
+          setDelta(500);
+        } else {
+          setIndex(prevIndex => prevIndex + 1);
+        }
+      }
 
     return (
         <section className="banner" id="home">
@@ -26,13 +51,13 @@ export const Banner = () => {
                 <Row className='align-items-center'>
                     <Col xs={12} md={6} xl={7}>
                         <span className='tagline'>Welcome to my Portfolio!</span>
-                        <h1>{`Hi, I'm Puyu(Jeff) Li`}<span className='wrap'>AI Developer</span></h1>
+                        <h1>{`Hi, I'm Puyu(Jeff) Li `}<span className='wrap'>{text}</span></h1>
                         <p>I am a passionate AI developer with a strong background in computer science and mathematics. I am proficient in machine learning, deep learning, and computer vision. I am also experienced in software development and data analysis. I am looking for opportunities to apply my skills to solve real-world problems.</p>
                         <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
                     </Col>
-                    <Col xs={12} md={6} xl={5}>
+                    {/* <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt="Header Img" />
-                    </Col>
+                    </Col> */}
                 </Row>
             </Container>
         </section>
--- myportfolio/src/components/NavBar.js
+++ myportfolio/src/components/NavBar.js
@@ -3,7 +3,6 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/img/logo.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
 export const NavBar = () => {
     const [scrolled, setScrolled] = useState(false);
     const [activeLink, setActiveLink] = useState('home');
