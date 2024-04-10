c5d2d5c464b3f9d1884bf8c8f7cb6675c08ab803
PUYU LI (Jeff) <lpy547898@gmail.com>
2024-04-10 16:23:28 +1000

    projects finished

--- myportfolio/src/components/NavBar.js
+++ myportfolio/src/components/NavBar.js
@@ -1,44 +1,46 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/img/logo.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
+import resume from '../assets/files/Jeff Li - Resume.pdf';
 export const NavBar = () => {
-    const [scrolled, setScrolled] = useState(false);
-    const [activeLink, setActiveLink] = useState('home');
-    useEffect(() => {
-        window.addEventListener('scroll', () => {
-            if (window.scrollY > 50) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        });
-    }, []);
-    const onUpdateActiveLink = (link) => {
-        setActiveLink(link);
-    }
-    return (
-        <Navbar expand="lg" className={scrolled?"scrolled":""}>
+  const [scrolled, setScrolled] = useState(false);
+  const [activeLink, setActiveLink] = useState('home');
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    });
+  }, []);
+  const onUpdateActiveLink = (link) => {
+    setActiveLink(link);
+  }
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
-        <Navbar.Brand href="#home"><img src={logo} alt="Logo"/></Navbar.Brand>
+        <Navbar.Brand href="#home"><img src={logo} alt="Logo" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"><span className='navbar-toggler-icon'></span></Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className={activeLink==='home'?'active navbar-link':'navbar-link'} onClick={()=>onUpdateActiveLink('home')}>Home</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink==='skills'?'active navbar-link':'navbar-link'} onClick={()=>onUpdateActiveLink('skills')}>Skills</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink==='projects'?'active navbar-link':'navbar-link'} onClick={()=>onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            <Nav.Link href={resume} className={activeLink === 'resume' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('resume')} download>Resume</Nav.Link>
           </Nav>
           <span className='navbar-text'>
             <div className='social-icon'>
-            <a href="https://www.linkedin.com/in/puyuli/"><img src={navIcon1} alt=""/></a>
-            <a href="https://github.com/lpy5618"><img src={navIcon2} alt=""/></a>
-            {/* <a href="#"><img src={navIcon3} alt=""/></a> */}
+              <a href="https://www.linkedin.com/in/puyuli/"><img src={navIcon1} alt="" /></a>
+              <a href="https://github.com/lpy5618"><img src={navIcon2} alt="" /></a>
+              {/* <a href="#"><img src={navIcon3} alt=""/></a> */}
             </div>
-            <button className='connect-btn' onClick={()=>console.log('connect')}><span>Let's Connect</span></button>
+            <button className='connect-btn' onClick={() => console.log('connect')}><span>Let's Connect</span></button>
           </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    );
+  );
 }
--- myportfolio/src/components/Skills.js
+++ myportfolio/src/components/Skills.js
@@ -32,34 +32,34 @@ export const Skills = () => {
             <Container>
                 <Row>
                     <Col>
-                    <div className='skill-bx'>
-                        <h2>Skills</h2>
-                        <p>Here are some of the skills I have acquired over the years.</p>
-                        <Carousel responsive={responsive} infinite={true} className='skill-slider'>
-                            <div className='item'>
-                                <img src={meter1} alt="Image"/>
-                                <h5>AI Development</h5>
-                            </div>
-                            <div className='item'>
-                                <img src={meter2} alt="Image"/>
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className='item'>
-                                <img src={meter3} alt="Image"/>
-                                <h5>Python</h5>
-                            </div>
-                            <div className='item'>
-                                <img src={meter1} alt="Image"/>
-                                <h5>Python</h5>
-                            </div>
-                        </Carousel>
-                        
-                    </div>
+                        <div className='skill-bx'>
+                            <h2>Skills</h2>
+                            <p>Here are some of the skills I have acquired over the years.</p>
+                            <Carousel responsive={responsive} infinite={true} className='skill-slider'>
+                                <div className='item'>
+                                    <img src={meter1} alt="Image" />
+                                    <h5>AI Development</h5>
+                                </div>
+                                <div className='item'>
+                                    <img src={meter2} alt="Image" />
+                                    <h5>Web Development</h5>
+                                </div>
+                                <div className='item'>
+                                    <img src={meter3} alt="Image" />
+                                    <h5>Python</h5>
+                                </div>
+                                <div className='item'>
+                                    <img src={meter1} alt="Image" />
+                                    <h5>Python</h5>
+                                </div>
+                            </Carousel>
+
+                        </div>
 
                     </Col>
                 </Row>
             </Container>
-            <img src={colorSharp} alt="Color Sharp" className='background-image-left'/>
+            <img src={colorSharp} alt="Color Sharp" className='background-image-left' />
         </section>
     )
 }
